f6492479d85a8740c4d8f2bbef04bfd14e435b21
Vardhini_P <vardhinikapardi@gmail.com>
2024-09-09 16:11:14 +0530

    Create 2886_Change_Data_Type.py

---
+++ 2886_Change_Data_Type.py
@@ -0,0 +1,35 @@
+/*
+DataFrame students
++-------------+--------+
+| Column Name | Type   |
++-------------+--------+
+| student_id  | int    |
+| name        | object |
+| age         | int    |
+| grade       | float  |
++-------------+--------+
+Write a solution to correct the errors:
+The grade column is stored as floats, convert it to integers.
+Input:
+DataFrame students:
++------------+------+-----+-------+
+| student_id | name | age | grade |
++------------+------+-----+-------+
+| 1          | Ava  | 6   | 73.0  |
+| 2          | Kate | 15  | 87.0  |
++------------+------+-----+-------+
+Output:
++------------+------+-----+-------+
+| student_id | name | age | grade |
++------------+------+-----+-------+
+| 1          | Ava  | 6   | 73    |
+| 2          | Kate | 15  | 87    |
++------------+------+-----+-------+
+*/
+
+# Solution:
+import pandas as pd
+
+def changeDatatype(students: pd.DataFrame) -> pd.DataFrame:
+    students['grade'] = students['grade'].astype(int)
+    return students
