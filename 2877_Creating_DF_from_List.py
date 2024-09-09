f767db52a7e327187d5adeeb5053068585e3ff69
Vardhini_P <vardhinikapardi@gmail.com>
2024-09-09 13:05:24 +0530

    Create 2877_Creating_DF_from_List.py

---
+++ 2877_Creating_DF_from_List.py
@@ -0,0 +1,26 @@
+/*
+Write a solution to create a DataFrame from a 2D list called student_data. This 2D list contains the IDs and ages of some students.
+The DataFrame should have two columns, student_id and age, and be in the same order as the original 2D list.
+Input:
+student_data:
+[
+  [1, 15],
+  [2, 11],
+  [3, 11],
+  [4, 20]
+]
+Output:
++------------+-----+
+| student_id | age |
++------------+-----+
+| 1          | 15  |
+| 2          | 11  |
+| 3          | 11  |
+| 4          | 20  |
++------------+-----+
+*/
+# Solution:
+import pandas as pd
+
+def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
+    return pd.DataFrame(student_data, columns = ['student_id','age'])
