2dd6e89f341055b25be46a4314e9989de3883612
Vardhini_P <vardhinikapardi@gmail.com>
2024-10-03 08:07:35 +0530

    Create 196_Delete_Duplicate_Emails.py

---
+++ 196_Delete_Duplicate_Emails.py
@@ -0,0 +1,32 @@
+/*
+Write a solution to delete all duplicate emails, keeping only one unique email with the smallest id.
+
+For Pandas users, please note that you are supposed to modify Person in place.
+
+After running your script, the answer shown is the Person table. The driver will first compile and run your piece of code and then show the Person table. The final order of the Person table does not matter.
+
+Input: 
+Person table:
++----+------------------+
+| id | email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
+| 3  | john@example.com |
++----+------------------+
+Output: 
++----+------------------+
+| id | email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
++----+------------------+
+Explanation: john@example.com is repeated two times. We keep the row with the smallest Id = 1.
+*/
+
+# Solution:
+import pandas as pd
+
+def delete_duplicate_emails(person: pd.DataFrame) -> None:
+    person.sort_values('id', inplace=True)
+    person.drop_duplicates(subset='email', keep='first', inplace=True)                                                                                   
