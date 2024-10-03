cd3f87ed7eb4f6064c7a8b0d80a0b2ee81304cca
Vardhini_P <vardhinikapardi@gmail.com>
2024-10-03 07:43:41 +0530

    Create 2356_Number_of_Unique_Subjects_Taught_by_Each_Teacher.py

---
+++ 2356_Number_of_Unique_Subjects_Taught_by_Each_Teacher.py
@@ -0,0 +1,42 @@
+/*
+Write a solution to calculate the number of unique subjects each teacher teaches in the university.
+
+Return the result table in any order.
+
+Input: 
+Teacher table:
++------------+------------+---------+
+| teacher_id | subject_id | dept_id |
++------------+------------+---------+
+| 1          | 2          | 3       |
+| 1          | 2          | 4       |
+| 1          | 3          | 3       |
+| 2          | 1          | 1       |
+| 2          | 2          | 1       |
+| 2          | 3          | 1       |
+| 2          | 4          | 1       |
++------------+------------+---------+
+Output:  
++------------+-----+
+| teacher_id | cnt |
++------------+-----+
+| 1          | 2   |
+| 2          | 4   |
++------------+-----+
+Explanation: 
+Teacher 1:
+  - They teach subject 2 in departments 3 and 4.
+  - They teach subject 3 in department 3.
+Teacher 2:
+  - They teach subject 1 in department 1.
+  - They teach subject 2 in department 1.
+  - They teach subject 3 in department 1.
+  - They teach subject 4 in department 1.
+
+*/
+
+# Solution:
+import pandas as pd
+
+def count_unique_subjects(teacher: pd.DataFrame) -> pd.DataFrame:
+    return teacher.groupby('teacher_id').agg(cnt=('subject_id', 'nunique')).reset_index()
